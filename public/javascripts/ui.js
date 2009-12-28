252a6935cbb10049b80bfbe893e688dd41eb063c
Jeremy Roman <jeremy@jeremyroman.com>
2009-12-28 10:09:07 -0500

    implementing AJAX functionality

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -5,17 +5,21 @@ function initializePlan(root) {
 	
 	$(".term", root).sortable({ items:'.term-course', connectWith:'.term',
 	 	update: function(ev, ui) {
-			if (console) {
-				console.log(this);
-				console.log([ev, ui]);
-			}
+			id_match = $(this).closest("div.ui-tabs-panel").attr("id").match(/(\d+)$/);
+			if (!id_match) return;
+			plan_id = id_match[1];
+			
+			$.doTimeout('push-and-reload-plan-'+plan_id, 70, function() {
+				console.log($(".ui-tabs-panel:not(.ui-tabs-panel-hide)"));
+				$(".tabs").tabs('load', $(".tabs").tabs('option', 'selected'));
+			});
 		}
 	});
 	
 	$(".term-course", root).mousedown(function() {
 		$(".term-course").removeClass("ui-state-highlight");
 		$(this).addClass("ui-state-highlight");
-		id = $(this).children("input[name=id]").val();
+		id = $(this).children("input[name=cid]").val();
 		$("#portlet-info .portlet-content").load('/courses/' + id);
 	});
 }
@@ -28,9 +32,13 @@ $(function() {
 	$(".tabs").tabs({
 		load: function(ev, ui) {
 			initializePlan(ui.panel);
+			
+			// try to make the same course selected
+			sel_id = $("#portlet-info input[name=course_id]").val();
+			if(!sel_id) return;
+			$("input[name=cid][value="+sel_id+"]", ui.panel).parent().addClass("ui-state-highlight");
 		},
 		show: function(ev, ui) {
-			console.log([ev, ui]);
 			id_match =  ui.tab.href.match(/(\d+)$/);
 			if (!id_match) return;
 			
