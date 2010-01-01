9ef893949806e92f881cd8ac99226c40b4a47276
Jeremy Roman <jeremy@jeremyroman.com>
2010-01-01 01:30:11 -0500

    added comment explaining why caches_page was not used

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,10 @@
 # fit anywhere else. This includes things like serving the
 # main UI.
 class MainController < ApplicationController
+  # this could also be done as caches_page,
+  # but then I'd have to remember to remove
+  # index.html whenever I switched to the
+  # development environment. :)
   caches_action :index
   
   # Serves the main UI.
