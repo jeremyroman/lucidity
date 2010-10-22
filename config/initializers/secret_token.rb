c9668dba1001c288fa35866d8ff5375e2baa6dd0
Jeremy Roman <jeremy@jeremyroman.com>
2010-10-22 18:16:32 -0400

    in with the new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lucidity::Application.config.secret_token = '3826ebc82bad7722a881fa639d6c00f57b6547b3fb04d465ec211fc36127ea0e3248581854ae19e37be29583e1c3602412754deada03cc995355b06b933f9822'
